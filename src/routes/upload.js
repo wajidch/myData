795a24a19b28a2513c6dbae65c2278ebc658c26c
neomeric <neomeric>
2019-03-07 09:45:13 +0500

    .env file

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -11,31 +11,31 @@ const upload = require('../utilities/upload');
 
 module.exports = [
 // Upload file
-    {
-        method: 'POST',
-        path: config.apiPrefix + '/upload',
-        config: {
-            description: 'Upload File',
-            notes: 'Upload file to the S3 server.',
-            tags: ['api', 'Upload'],
+//     {
+//         method: 'POST',
+//         path: config.apiPrefix + '/upload',
+//         config: {
+//             description: 'Upload File',
+//             notes: 'Upload file to the S3 server.',
+//             tags: ['api', 'Upload'],
            
          
-  handler: async (req, h) => {
-    const { payload } = req
-    const response = upload.handleFileUpload(payload.file)
-    return response
-  },
-            validate: {
-                payload: Joi.object({
-                    file: Joi.any().meta({swaggerType: 'file'}).description('.png|.jpg file').required()
-                }),
-                failAction: (request, reply, source, err) => {
-                    reply(responses.makeMessageResponse(false, statusCodes.BAD_REQUEST, err.message.replace(/[^a-zA-Z ]/g, '')));
-                }
-            },
-            payload: plugins.uploadPayload,
-            timeout: plugins.uploadTimeOut,
-            plugins: plugins.swaggerPluginFile
-        }
-    }
+//   handler: async (req, h) => {
+//     const { payload } = req
+//     const response = upload.handleFileUpload(payload.file)
+//     return response
+//   },
+//             validate: {
+//                 payload: Joi.object({
+//                     file: Joi.any().meta({swaggerType: 'file'}).description('.png|.jpg file').required()
+//                 }),
+//                 failAction: (request, reply, source, err) => {
+//                     reply(responses.makeMessageResponse(false, statusCodes.BAD_REQUEST, err.message.replace(/[^a-zA-Z ]/g, '')));
+//                 }
+//             },
+//             payload: plugins.uploadPayload,
+//             timeout: plugins.uploadTimeOut,
+//             plugins: plugins.swaggerPluginFile
+//         }
+//     }
 ];
