712d852ff888bfc7188b5211ddced177c4a4d24a
neomeric <neomeric>
2019-03-23 12:38:52 +0500

    .env

--- configs/config.js
+++ configs/config.js
@@ -7,9 +7,9 @@ module.exports = {
     projectName: process.env.PROJECT_NAME,
     secretKey: process.env.SERVER_SECRET_KEY,
     rootPath: path.join(__dirname, '/..'),
-    //serverUrl: 'http://134.209.29.69:5001',
-    serverUrl: 'localhost:5002',
-    port: '5002',
+    serverUrl: 'http://134.209.29.69:5001',
+    //serverUrl: 'localhost:4000',
+    port: '5001',
     environment: process.env.NODE_ENV,
     mailer: {
         service: process.env.MAILER_PROVIDER,
